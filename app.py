930b8c80264bed336c88f0d0e87f2e1e14dd9403
Kiran.Sharma <kiran.sharma@Services-MacBook-Pro.local>
2025-12-04 18:17:39 +0530

    Added new code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Azure CI/CD demo! - Content now updated"
+    return "Hello from Azure CI/CD demo! - Content now updated.. again"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
